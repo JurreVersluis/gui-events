0121b2f4c26117803c60473df00d218e1f828600
Idioot <99066617@mydavinci.nl>
2022-03-15 11:37:02 +0100

    test

---
+++ clickerv4.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+getal1 = 0
+lastpressed = ''
+
+def check():
+    if getal1 > 0:
+        window.config(bg="green")
+    elif getal1 < 0:
+        window.config(bg="red")
+    else:
+        window.config(bg="grey")
+
+
+def upnumber(event):
+    global getal1, lastpressed
+    getal1 += 1
+    lastpressed = 'up'
+    getal.set(getal1)
+    check()
+
+
+def downnumber(event):
+    global getal1, lastpressed
+    getal1 -= 1
+    lastpressed = 'down'
+    getal.set(getal1)
+    check()
+
+
+def enter(event):
+    window.config(bg="yellow")
+
+
+def leave(event):
+    check()
+
+
+def multiplier(event):
+    global getal1
+    if lastpressed == 'up':
+        getal1 *= 3
+    elif lastpressed == 'down':
+        getal1 /= 3
+    getal.set(getal1)
+
+
+window = tk.Tk()
+window.title('Clicker v1')
+window.geometry("400x600")
+window.config(bg='grey')
+
+window.bind("<Up>", upnumber)
+window.bind("<+>", upnumber)
+window.bind("<Down>", downnumber)
+window.bind("<minus>", downnumber)
+window.bind("<space>", multiplier)
+getal = tk.StringVar()
+getal.set(getal1)
+
+button = tk.Button(window)
+button.pack(ipadx="150",ipady="15", pady="100")
+button.configure(text="Up",bg='white', command=upnumber)
+
+label = tk.Label(window, textvariable=getal)
+label.pack(ipadx="150",ipady="15")
+label.bind('<Enter>', enter)
+label.bind('<Leave>', leave)
+label.bind('<Double-Button-1>', multiplier)
+label.configure(bg='white')
+
+button = tk.Button(window)
+button.pack(ipadx="150",ipady="15", pady="100")
+button.configure(text="Down",bg='white', command=downnumber)
+
+
+
+window.mainloop()
